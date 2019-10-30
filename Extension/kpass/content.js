35fbe4632f5c760ff4ef49112aa4740c6ab68036
aliensmart <kaoua17@gmail.com>
2019-10-30 10:09:27 -0400

    webrequest added

---
+++ Extension/kpass/content.js
@@ -0,0 +1,25 @@
+// Send a message containing the page details back to the event page
+chrome.runtime.sendMessage({
+    'title': document.title,
+    'url': window.location.href,
+});
+
+// //start connection in content script
+// let contentPort = chrome.runtime.connect({
+//     name: 'background-content'
+//  });
+ 
+//  //Listen for runtime message
+//  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+//     //Retrieve offset dimension
+//     console.log(message)
+//     if(message.action === 'GET_DIMENSION') {
+//        contentPort.postMessage({
+//           type: 'DIMENSION', 
+//           payload: {
+//              height: document.body.offsetHeight,
+//              width: document.body.offsetWidth       
+//           }
+//        });
+//     }
+//  });
--- Extension/kpass/second.js
+++ Extension/kpass/second.js
@@ -11,6 +11,16 @@ function clearLocalStorage(){
     })
 }
 
+//This callback function is called when the content script has been injected and returned its results
+
+function onPageDetailsReceived(pageDetails){
+    document.getElementById('title').value = pageDetails.title;
+    document.getElementById('url').value = pageDetails.url;
+
+}
+
+// Global reference to the status display SPAN
+var statusDisplay = null;
 
 
 
@@ -18,3 +28,20 @@ chrome.storage.local.get(['key'], function(result) {
     console.log(result.key)
     
   })
+
+
+      // When the popup HTML has loaded
+window.addEventListener('load', function(evt) {
+    // Cache a reference to the status display SPAN
+    statusDisplay = document.getElementById('status-display');
+    // Handle the bookmark form submit event with our addBookmark function
+    // document.getElementById('addbookmark')
+    //         .addEventListener('submit', addBookmark);
+    // Get the event page
+    chrome.runtime.getBackgroundPage(function(eventPage) {
+        // Call the getPageInfo function in the event page, passing in
+        // our onPageDetailsReceived function as the callback. This
+        // injects content.js into the current tab's HTML
+        eventPage.getPageDetails(onPageDetailsReceived)
+    });
+});
